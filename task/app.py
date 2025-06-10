eaea70d305b387b4c42778f67dd3cb661b7d57d0
“Khshanovskyi” <pavlo_khshanovskyi@epam.com>
2025-06-10 12:30:30 +0300

    Completed

--- task/app.py
+++ task/app.py
@@ -8,25 +8,51 @@ from task.models.role import Role
 
 
 async def start(stream: bool) -> None:
-    #TODO:
     # 1. Create DialClient
-    #    - endpoint: constants.DIAL_ENDPOINT
-    #    - deployment_name: available gpt model. Sample: `gpt-4o`
-    #      (you can get available deployment_name via https://ai-proxy.lab.epam.com/openai/models
-    #       you can import Postman collection to make a request, file in the project root `dial-basics.postman_collection.json`
-    #       don't forget to add your API_KEY)
-    #    - api_key: YOUR_API_KEY
-    # 2. Create Conversation object
-    # 3. Get System prompt from console or use default -> constants.DEFAULT_SYSTEM_PROMPT and add to conversation
-    #    messages. To do that use the `input()` function
-    # 4. Use infinite cycle (while True) and get yser message from console
-    # 5. If user message is `exit` then stop the loop
-    # 6. Add user message to conversation history (role 'user')
-    # 7. If `stream` param is true -> call DialClient#get_completion()
-    #    else -> call DialClient#stream_completion()
-    # 8. Add generated message to history
+    client = DialClient(
+        endpoint=DIAL_ENDPOINT,
+        deployment_name='gpt-4o',
+        api_key='YOUR_API_KEY',
+    )
 
-    pass
+    # 2. Create conversation
+    conversation = Conversation()
+
+    # 3. Get system prompt
+    print("Provide System prompt or press 'enter' to continue.")
+    prompt = input("> ").strip()
+    
+    if prompt:
+        conversation.add_message(Message(Role.SYSTEM, prompt))
+        print("System prompt successfully added to conversation.")
+    else:
+        conversation.add_message(Message(Role.SYSTEM, DEFAULT_SYSTEM_PROMPT))
+        print(f"No System prompt provided. Will be used default System prompt: '{DEFAULT_SYSTEM_PROMPT}'")
+    
+    print()
+    
+    # 4. Main chat loop
+    print("Type your question or 'exit' to quit.")
+    while True:
+        user_input = input("> ").strip()
+    
+        # 5. Check for exit
+        if user_input.lower() == "exit":
+            print("Exiting the chat. Goodbye!")
+            break
+    
+        # 6. Add user message and get AI response
+        conversation.add_message(Message(Role.USER, user_input))
+
+        #7. Call LLM
+        print("AI:")
+        if stream:
+            ai_message = await client.stream_completion(conversation.get_messages())
+        else:
+            ai_message = client.get_completion(conversation.get_messages())
+
+        # Add assistant message to history
+        conversation.add_message(ai_message)
 
 asyncio.run(
     start(True)
