c1db35258652de82b41fa6d082f516c31af617af
“Khshanovskyi” <pavlo_khshanovskyi@epam.com>
2025-06-26 08:31:59 +0300

    Task

--- task/app.py
+++ task/app.py
@@ -1,7 +1,7 @@
 import asyncio
 
 from task.client import DialClient
-from task.constants import DEFAULT_SYSTEM_PROMPT, DIAL_ENDPOINT
+from task.constants import DEFAULT_SYSTEM_PROMPT, DIAL_ENDPOINT, API_KEY
 from task.models.conversation import Conversation
 from task.models.message import Message
 from task.models.role import Role
@@ -15,7 +15,7 @@ async def start(stream: bool) -> None:
     #      (you can get available deployment_name via https://ai-proxy.lab.epam.com/openai/models
     #       you can import Postman collection to make a request, file in the project root `dial-basics.postman_collection.json`
     #       don't forget to add your API_KEY)
-    #    - api_key: YOUR_API_KEY
+    #    - api_key: API_KEY
     # 2. Create Conversation object
     # 3. Get System prompt from console or use default -> constants.DEFAULT_SYSTEM_PROMPT and add to conversation
     #    messages. To do that use the `input()` function
--- task/constants.py
+++ task/constants.py
@@ -1,2 +1,5 @@
+import os
+
 DEFAULT_SYSTEM_PROMPT = "You are an assistant who answers concisely and informatively."
 DIAL_ENDPOINT = "https://ai-proxy.lab.epam.com/openai/deployments/{model}/chat/completions"
+API_KEY = os.getenv('DIAL_API_KEY', '')
